b20bf045a7bbafaea2ed0d30f8effe3beb25a775
Vitaliy Zvieriev <zverew91@gmail.com>
2020-10-22 03:31:01 +0300

    Добавляет разметку index.html и main.js

---
+++ task-2/task-2.js
@@ -0,0 +1,38 @@
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+/*
+const toggleUserState = (allUsers, userName, callback) => {
+  const updatedUsers = allUsers.map((user) =>
+    user.name === userName ? { ...user, active: !user.active } : user
+  );
+
+  callback(updatedUsers);
+};
+
+
+ // Сейчас работает так
+ 
+ toggleUserState(users, "Mango", logger);
+ toggleUserState(users, "Lux", logger);
+*/
+
+// ----- Promise -----
+const toggleUserState = (allUsers, userName) => {
+  return Promise.resolve(
+    allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    )
+  );
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+// Должно работать так
+
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
